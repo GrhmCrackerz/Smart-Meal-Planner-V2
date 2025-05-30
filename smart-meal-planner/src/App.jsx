30a2ade2177ad801162c5b618abc19c0fcb007cd
GrhmCrackerz <88797817+GrhmCrackerz@users.noreply.github.com>
2025-05-30 18:47:14 -0400

    first commit

---
+++ smart-meal-planner/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import WeeklyPlanner from './WeeklyPlanner'
+
+function App() {
+  return (
+    <div>
+      <h1>Smart Meal Planner & Grocery List Generator</h1>
+      <WeeklyPlanner />
+    </div>
+  )
+
+}
+
+export default App
