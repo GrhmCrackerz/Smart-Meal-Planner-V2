ff54e182e606c9c4282cdae15487ae4114511b67
GrhmCrackerz <88797817+GrhmCrackerz@users.noreply.github.com>
2025-06-02 08:47:11 -0400

    More changes

--- smart-meal-planner/vite.config.js
+++ smart-meal-planner/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/smart-meal-planner/',
+  base: '/Smart-Meal-Planner/',
 })
